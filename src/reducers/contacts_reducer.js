a9fdf39cb27ab27ee31c560c347c33938a6b0cde
Michael Carniato <mcarniato1991@gmail.com>
2017-08-09 22:00:59 +1000

    refactored contacts reducer

--- src/reducers/contacts_reducer.js
+++ src/reducers/contacts_reducer.js
@@ -1,14 +1,14 @@
 import _ from 'lodash';
 import { GET_ALL_CONTACTS, DELETE_CONTACT, UPDATE_CONTACT } from '../actions/types';
 
-export default function(state = null, action ) {
+export default function(state = {}, action ) {
   switch(action.type) {
     case GET_ALL_CONTACTS:
       return _.mapKeys(action.payload, 'id');
     case DELETE_CONTACT:
       return _.omit(state, action.payload);
     case UPDATE_CONTACT:
-      return _.mapKeys(action.payload, 'id');
+      return { ...state, [action.payload.id]: action.payload }
     default:
       return state;
   }
