b824b309b7de4df9685d3070d60042d679bdf1ed
Michael Carniato <mcarniato1991@gmail.com>
2017-08-12 21:14:35 +1000

    added select org box to create new contact component

--- src/components/contacts/create_contact.js
+++ src/components/contacts/create_contact.js
@@ -3,8 +3,14 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Field, reduxForm } from 'redux-form';
 import { createContact } from '../../actions/contacts/index';
+import { getAllOrgs } from '../../actions/organisations/index';
+import _ from 'lodash';
 
 class CreateContact extends Component {
+
+  componentWillMount() {
+    this.props.getAllOrgs()
+  }
 
   renderField(field) {
     const { type, placeholder } = field;
@@ -17,6 +23,26 @@ class CreateContact extends Component {
         <div className="danger-text">
           { touched ? error : '' }
         </div>
+      </div>
+    )
+  }
+
+  renderDropDown(field) {
+    const { name, label, ID, organisations  } = field;
+    _.map(organisations, org => {
+      console.log(org)
+    })
+
+    return (
+      <div className="form-group">
+        <label htmlFor={ ID }>{ label }</label>
+        <select className="form-control" id="sel1">
+          { _.map(organisations, org => {
+            return (
+              <option key={ org.id } value={ org.id }>{ org.name }</option>
+            )
+          }) }
+        </select>
       </div>
     )
   }
@@ -54,6 +80,13 @@ class CreateContact extends Component {
             component={ this.renderField }
             placeholder="1300 batman"
           />
+          <Field
+          name="selectOrg"
+          label="Select Organisation"
+          component={ this.renderDropDown }
+          ID="org"
+          organisations={ this.props.orgs }
+          />
           <button type="submit" className="btn btn-primary cursor-pointer">Create New Contact</button>
         </form>
       </div>
@@ -80,9 +113,14 @@ function validate(values) {
   return errors;
 }
 
+function mapStateToProps( { orgs } ) {
+  console.log(orgs)
+  return { orgs: orgs }
+}
+
 export default reduxForm({
   validate,
   form: 'createContact'
 })(
-  connect(null, { createContact })(CreateContact)
+  connect(mapStateToProps, { createContact, getAllOrgs })(CreateContact)
 )
