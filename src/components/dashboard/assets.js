73a3b1533f54bb11b52a3c78b6b1332221b1c8ad
Michael Carniato <mcarniato1991@gmail.com>
2017-08-16 00:17:45 +1000

    added mapstatetoprops in assets

--- src/components/dashboard/assets.js
+++ src/components/dashboard/assets.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getAllAssets } from '../../actions/assets/index';
+import _ from 'lodash';
+import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
+import { Link } from 'react-router-dom';
 
 class Assets extends Component {
+
+  componentWillMount() {
+    this.props.getAllAssets()
+  }
+
   render() {
     return (
       <div>
-        Asset Component
+        <ul>
+        {console.log(this.props.assets)}
+          { _.map(this.props.assets, asset => {
+            <li key={ asset.id }>{ asset.name }</li>
+          }) }
+        </ul>
       </div>
     )
   }
 }
 
-export default Assets;
+function mapStateToProps(state) {
+  return { assets: state.assets }
+}
+
+export default connect(mapStateToProps, { getAllAssets })(Assets);
